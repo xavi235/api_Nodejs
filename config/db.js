42d8bed36f624ae394f84e6db3583c244250bb75
xavi235 <linkshaoran24@gmail.com>
2025-06-27 21:37:53 -0400

    corecion

--- config/db.js
+++ config/db.js
@@ -1,14 +1,19 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
 });
 
-module.exports = pool;
+connection.connect((err) => {
+  if (err) {
+    console.error('❌ Error al conectar a MySQL:', err);
+    process.exit(1);
+  }
+  console.log('📦 Conectado a la base de datos MySQL');
+});
+
+module.exports = connection;
